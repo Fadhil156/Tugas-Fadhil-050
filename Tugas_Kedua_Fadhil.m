7ff071e5a5fea242dfa4da1dd19f685fbf964164
Fadhil Hasan <147014839+Fadhil156@users.noreply.github.com>
2023-10-25 09:27:33 +0700

    Add files via upload

---
+++ Tugas_Kedua_Fadhil.m
@@ -0,0 +1,29 @@
+%nama : Fadhil Hasan 
+%Nim  : 112022050                                       kelas:B
+%Soal 2
+
+clc
+clear
+
+% a = matriks identitas 5x5
+% a = eye(_)
+
+a = eye(5,5)
+
+% b = matriks 3x3 elemen 0
+% b= zeros(_)
+
+b = zeros(3,3)
+
+% c = matriks 4x4 elemen 1
+% c = ones9(_)
+
+c = ones(4,4)
+
+% d = matriks 3x4 distribusi antara elemen 0 dan 1
+% d = rand(_)
+
+d = rand (3,4)
+
+% e= matriks 3x4 terdistribusi normal
+  e = randn(3,4)
